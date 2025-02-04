efb0a95186e5f483edc2d7367031980b614c1bbc
cyanea-bt <150618858+cyanea-bt@users.noreply.github.com>
2025-02-04 07:28:25 +0100

    initial commit based on https://github.com/cyanea-bt/SilentPatchYRC

---
+++ README.md
@@ -0,0 +1,4 @@
+```
+git clone --recursive https://github.com/cyanea-bt/Yakuza3HeatFix.git
+premake5 vs2019
+```
---
+++ source/VersionInfo.lua
@@ -0,0 +1,8 @@
+defines { 
+    "rsc_FullName=\"Yakuza3HeatFix\"",
+    "rsc_MinorVersion=0",
+    "rsc_RevisionID=1",
+    "rsc_BuildID=0",
+    "rsc_Copyright=\"2025\"",
+    "rsc_UpdateURL=\"https://github.com/cyanea-bt/Yakuza3HeatFix/releases\""
+}
