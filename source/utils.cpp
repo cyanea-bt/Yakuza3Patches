eab92563c53e6d2b6a075be7fe9cd498f5531d72
cyanea-bt <150618858+cyanea-bt@users.noreply.github.com>
2025-02-26 01:44:33 +0100

    add string helper functions to utils.cpp

--- source/utils.cpp
+++ source/utils.cpp
@@ -82,4 +82,68 @@ namespace utils {
 			logfileMap[channel].close();
 		}
 	}
+
+	//
+	//
+	//
+
+	// ref: https://gist.github.com/GenesisFR/cceaf433d5b42dcdddecdddee0657292
+	string replaceAll(const string &str, const string &from, const string &to) {
+		string newStr(str);
+		size_t start_pos = 0;
+		while ((start_pos = newStr.find(from, start_pos)) != string::npos) {
+			newStr.replace(start_pos, from.length(), to);
+			start_pos += to.length(); // Handles case where 'to' is a substring of 'from'
+		}
+		return newStr;
+	}
+
+	void replaceAllByRef(string &str, const string &from, const string &to) {
+		size_t start_pos = 0;
+		while ((start_pos = str.find(from, start_pos)) != string::npos) {
+			str.replace(start_pos, from.length(), to);
+			start_pos += to.length(); // Handles case where 'to' is a substring of 'from'
+		}
+	}
+
+
+	// ref: https://stackoverflow.com/a/63050738
+	string_view ltrim(string_view str)
+	{
+		const auto pos(str.find_first_not_of(" \t\n\r\f\v"));
+		str.remove_prefix(min(pos, str.length()));
+		return str;
+	}
+
+	string_view rtrim(string_view str)
+	{
+		const auto pos(str.find_last_not_of(" \t\n\r\f\v"));
+		str.remove_suffix(min(str.length() - pos - 1, str.length()));
+		return str;
+	}
+
+	string_view trim(string_view str)
+	{
+		str = ltrim(str);
+		str = rtrim(str);
+		return str;
+	}
+
+
+	// ref: https://en.cppreference.com/w/cpp/string/byte/toupper
+	string uppercase(string_view str) {
+		stringstream ss;
+		for (const unsigned char &c : str) {
+			ss << static_cast<char>(std::toupper(c));
+		}
+		return ss.str();
+	}
+
+	string lowercase(string_view str) {
+		stringstream ss;
+		for (const unsigned char &c : str) {
+			ss << static_cast<char>(std::tolower(c));
+		}
+		return ss.str();
+	}
 }
--- source/utils.h
+++ source/utils.h
@@ -9,6 +9,14 @@ namespace utils {
 	void Log(std::string_view msg, const int channel = -1);
 	void Log(std::string_view msg, const bool close, const int channel = -1);
 
+	std::string replaceAll(const std::string &str, const std::string &from, const std::string &to);
+	void replaceAllByRef(std::string &str, const std::string &from, const std::string &to);
+	std::string_view ltrim(std::string_view str);
+	std::string_view rtrim(std::string_view str);
+	std::string_view trim(std::string_view str);
+	std::string lowercase(std::string_view str);
+	std::string uppercase(std::string_view str);
+
 	// Can't seem to get the correct address of a function at runtime without this
 	// ref: Utils/Trampoline.h
 	template<typename Func>
